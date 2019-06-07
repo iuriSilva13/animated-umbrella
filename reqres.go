c8260fcb83949c0aaf59d487f96ad3092c1a3cfc
Vladimir Vasconcelos <vladimir.canellas@gmail.com>
2019-06-07 11:11:05 -0300

    Resposta nome reqres
    e implementado struct para agrupar os campos de dados e receber os
    valores pelo json,o unmarshal para pegar os dados do json convertendo
    para objetos,faz uma chamada na main para outra funcao que recebe o
    endereco dos dados e espera o retorno de um array de string,percorre o
    array data em um for e faz uma nova requisicao para pegar um unico id
    de cada usuario.

--- reqres.go
+++ reqres.go
@@ -1,13 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type dadosData struct {
+	ValoresData dadosUsuario `json:"data"`
+}
+type dadosUsuario struct {
+	Id         int    `json:"id"`
+	First_name string `json:"first_name"`
+	Last_name  string `json:"last_name"`
+	Avatar     string `json:"avatar"`
+}
+type respostaUsuarios struct {
+	Page        int            `json:"page"`
+	Per_page    int            `json:"per_page"`
+	Total       int            `json:"total"`
+	Total_pages int            `json:"total_pages"`
+	Data        []dadosUsuario `json:"data"`
+}
+
 func main() {
-	resposta, _ := http.Get("https://reqres.in/api/users/2")
+	obterNomes("https://reqres.in/api/users?page=4")
+}
+func obterNomes(endpoint string) []string {
+	var resultado []string
+
+	resposta, _ := http.Get(endpoint)
 	body, _ := ioutil.ReadAll(resposta.Body)
-	fmt.Printf("status da resposta: %s\n", body)
+	var resp respostaUsuarios
+	_ = json.Unmarshal(body, &resp)
+
+	for _, usuario := range resp.Data {
+		endPoint := "https://reqres.in/api/users/" + fmt.Sprintf("%+v", usuario.Id)
+		resposta2, _ := http.Get(endPoint)
+		body2, _ := ioutil.ReadAll(resposta2.Body)
+		var dadosRecebios dadosData
+		erro := json.Unmarshal(body2, &dadosRecebios)
+
+		if erro != nil {
+			fmt.Println(erro)
+		}
+
+		fmt.Printf("o nome completo do usuario é:%s\n", usuario.First_name+" "+usuario.Last_name)
+	}
+
+	return resultado
+
 }
