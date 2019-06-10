b4b804d7cd6065926e126727ce1e6898f799ebd8
Vladimir Vasconcelos <vladimir.canellas@gmail.com>
2019-06-10 13:27:17 -0300

    Retornando variavel que nao era usada
    Na funcao main a chamada obterNomes e colocada em uma variavel,
    faco um for que ira printar os valores recebidos e na funcao
    obterNomes a variavel resultado que antes nao era usada agora
    eu uso ela para acrescentar os nomes dos usuarios.

--- reqres.go
+++ reqres.go
@@ -1,13 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
+type dadosData struct {
+	ValoresData dadosUsuario `json:"data"`
+}
+type dadosUsuario struct {
+	Id         int    `json:"id"`
+	First_name string `json:"first_name"`
+	Last_name  string `json:"last_name"`
+	Avatar     string `json:"avatar"`
+}
+type respostaUsuarios struct {
+	Page        int            `json:"page"`
+	Per_page    int            `json:"per_page"`
+	Total       int            `json:"total"`
+	Total_pages int            `json:"total_pages"`
+	Data        []dadosUsuario `json:"data"`
+}
+
 func main() {
-	resposta, _ := http.Get("https://reqres.in/api/users/2")
+	ReceberNomes := obterNomes("https://reqres.in/api/users?page=4")
+	for _, NomeCompleto := range ReceberNomes {
+		fmt.Printf("o nome completo do usuario é:%s\n", NomeCompleto)
+	}
+}
+func obterNomes(endpoint string) []string {
+	var resultado []string
+
+	resposta, _ := http.Get(endpoint)
 	body, _ := ioutil.ReadAll(resposta.Body)
-	fmt.Printf("status da resposta: %s\n", body)
+	var resp respostaUsuarios
+	_ = json.Unmarshal(body, &resp)
+
+	for _, usuario := range resp.Data {
+		endPoint := "https://reqres.in/api/users/" + fmt.Sprintf("%+v", usuario.Id)
+		respostaEndpoint, _ := http.Get(endPoint)
+		body2, _ := ioutil.ReadAll(respostaEndpoint.Body)
+		var dadosRecebios dadosData
+		_ = json.Unmarshal(body2, &dadosRecebios)
+
+		resultado = append(resultado, usuario.First_name+" "+usuario.Last_name)
+	}
+
+	return resultado
+
 }
