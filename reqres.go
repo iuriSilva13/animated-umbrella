9fc57a88789b6e1e490b83881aa208e0b4a80577
Vladimir Vasconcelos <vladimir.canellas@gmail.com>
2019-06-11 15:06:16 -0300

    Retorno nome completo
    Nome das structs,funcoes e variaveis foram trocadas,na funcao
    obterId agora a chamada pra funcao obterNomeCompleto e colocada
    na variavel Nome,a variavel ListaNome e usada pra acrescentar os
    nomes,a funcao obterNomeCompleto agora so recebe o endpoint como
    parametro e espera uma string de retorno e a variavel resultado
    agora nao existe mais e retorna o nome completo do usuario.

--- reqres.go
+++ reqres.go
@@ -7,55 +7,50 @@ import (
 	"net/http"
 )
 
-type dadosData struct {
-	ValoresData dadosUsuario `json:"data"`
-}
-type dadosUsuario struct {
+type Usuario struct {
 	Id         int    `json:"id"`
 	First_name string `json:"first_name"`
 	Last_name  string `json:"last_name"`
 	Avatar     string `json:"avatar"`
 }
+type respostaUsuario struct {
+	ValoresData Usuario `json:"data"`
+}
 type respostaUsuarios struct {
-	Page        int            `json:"page"`
-	Per_page    int            `json:"per_page"`
-	Total       int            `json:"total"`
-	Total_pages int            `json:"total_pages"`
-	Data        []dadosUsuario `json:"data"`
+	Page        int       `json:"page"`
+	Per_page    int       `json:"per_page"`
+	Total       int       `json:"total"`
+	Total_pages int       `json:"total_pages"`
+	Data        []Usuario `json:"data"`
 }
 
 func main() {
-	ReceberNomes := obterNomes("https://reqres.in/api/users?page=4")
-	for _, NomeCompleto := range ReceberNomes {
-		fmt.Printf("o nome completo do usuario é:%+v\n", NomeCompleto)
+	dadosRespostaUsuarios := obterId("https://reqres.in/api/users?page=4")
+	for _, RespostaNomeCompleto := range dadosRespostaUsuarios {
+		fmt.Printf("o nome completo do usuario é:%+v\n", RespostaNomeCompleto)
 	}
 }
-func obterNomes(endPoint string) []string {
-	var resultado []string
+func obterId(endPoint string) []string {
+	var ListaNomes []string
+
 	resposta, _ := http.Get(endPoint)
 	body, _ := ioutil.ReadAll(resposta.Body)
-	var resp respostaUsuarios
-	_ = json.Unmarshal(body, &resp)
+	var dadosRecebidos respostaUsuarios
+	_ = json.Unmarshal(body, &dadosRecebidos)
 
-	for _, usuario := range resp.Data {
-		Idusuarios := "https://reqres.in/api/users/" + fmt.Sprintf("%+v", usuario.Id)
-		resultado = obterEndpoint(Idusuarios, resp)
+	for _, usuario := range dadosRecebidos.Data {
+		Nome := obterNomeCompleto("https://reqres.in/api/users/" + fmt.Sprintf("%+v", usuario.Id))
+		ListaNomes = append(ListaNomes, Nome)
 	}
 
-	return resultado
-
+	return ListaNomes
 }
-func obterEndpoint(listaNomes string, dadosId respostaUsuarios) []string {
-	var resultado []string
-	respostaEndpoint, _ := http.Get(listaNomes)
+
+func obterNomeCompleto(ListaId string) string {
+	respostaEndpoint, _ := http.Get(ListaId)
 	body2, _ := ioutil.ReadAll(respostaEndpoint.Body)
-	var dadosRecebios dadosData
-	_ = json.Unmarshal(body2, &dadosRecebios)
+	var dadosUsuario respostaUsuario
+	_ = json.Unmarshal(body2, &dadosUsuario)
 
-	for _, usuario2 := range dadosId.Data {
-		resultado = append(resultado, usuario2.First_name+" "+usuario2.Last_name)
-	}
-
-	return resultado
-
+	return dadosUsuario.ValoresData.First_name + " " + dadosUsuario.ValoresData.Last_name
 }
