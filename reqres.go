7571e3170041b6250f04005a6cccb1fb17f80ce7
iuri silva <iuriksilv33@gmail.com>
2019-06-17 10:47:15 -0300

    Tratando a resposta do servidor remoto
    - mudanca de nomes
    Foram trocadas os nomes das structs,funcoes e variaveis.
    
    - tratando a resposta
    Na funcao main foi feito um tratamento com o tamanho da variavel
    listaNomes caso a resposta nao vir.

--- reqres.go
+++ reqres.go
@@ -8,34 +8,38 @@ import (
 	"time"
 )
 
-type Usuario struct {
+type reqres_data_user struct {
 	Id         int    `json:"id"`
 	First_name string `json:"first_name"`
 	Last_name  string `json:"last_name"`
 	Avatar     string `json:"avatar"`
 	Email      string `json:"email"`
 }
-type respostaUsuario struct {
-	ValoresData Usuario `json:"data"`
+type reqres_response_user struct {
+	DadosUsuario reqres_data_user `json:"data"`
 }
-type respostaUsuarios struct {
-	Page        int       `json:"page"`
-	Per_page    int       `json:"per_page"`
-	Total       int       `json:"total"`
-	Total_pages int       `json:"total_pages"`
-	Data        []Usuario `json:"data"`
+type reqres_response_users struct {
+	Page        int                `json:"page"`
+	Per_page    int                `json:"per_page"`
+	Total       int                `json:"total"`
+	Total_pages int                `json:"total_pages"`
+	Data        []reqres_data_user `json:"data"`
 }
 
 var httpClient = &http.Client{Timeout: time.Second * 2}
 
 func main() {
-	dadosRespostaUsuarios := obterId("https://reqres.in/api/users?page=4")
-	for _, RespostaNomeCompleto := range dadosRespostaUsuarios {
-		fmt.Printf("o nome completo do usuario é:%+v\n", RespostaNomeCompleto)
+	listaNomes := consultarNomesNoServidorRemoto("https://reqres.in/api/users?page=4")
+	if len(listaNomes) == 0 {
+		fmt.Println("Não recebi nenhuma resposta do servidor remoto")
+		return
+	}
+	for _, nomeCompleto := range listaNomes {
+		fmt.Printf("o nome completo do usuario é:%+v\n", nomeCompleto)
 	}
 }
 
-func obterId(endPoint string) []string {
+func consultarNomesNoServidorRemoto(endPoint string) []string {
 	var ListaNomes []string
 
 	req, _ := http.NewRequest("GET", endPoint, nil)
@@ -52,7 +56,7 @@ func obterId(endPoint string) []string {
 	}
 
 	body, _ := ioutil.ReadAll(Resposta.Body)
-	var dadosRecebidos respostaUsuarios
+	var dadosRecebidos reqres_response_users
 	_ = json.Unmarshal(body, &dadosRecebidos)
 
 	for _, usuario := range dadosRecebidos.Data {
@@ -78,8 +82,8 @@ func obterNomeCompleto(ListaId string) string {
 	}
 
 	body2, _ := ioutil.ReadAll(Resposta.Body)
-	var dadosUsuario respostaUsuario
-	_ = json.Unmarshal(body2, &dadosUsuario)
+	var resposta reqres_response_user
+	_ = json.Unmarshal(body2, &resposta)
 
-	return dadosUsuario.ValoresData.First_name + " " + dadosUsuario.ValoresData.Last_name
+	return resposta.DadosUsuario.First_name + " " + resposta.DadosUsuario.Last_name
 }
